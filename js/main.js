59e39d208bc45338a2c5fc72e7bd35dc8f90080e
kuny <kunyscrop@gmail.com>
2025-06-27 10:05:02 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Configuration Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDQHfXEcC-LgqbtYKpxoTmj0Ns5nmungwE",
+  authDomain: "mzikicd.firebaseapp.com",
+  projectId: "mzikicd",
+  storageBucket: "mzikicd.firebasestorage.app",
+  messagingSenderId: "656917047429",
+  appId: "1:656917047429:web:901057f18288feaba9e1ec",
+  databaseURL: "https://mzikicd-default-rtdb.firebaseio.com"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+// Connexion unique
+function signIn() {
+  const email = "kuny137@gmail.com";
+  const pass = "kuny137$KN";
+  auth.signInWithEmailAndPassword(email, pass)
+    .then(() => alert("✅ Connecté"))
+    .catch(err => alert("Erreur de connexion contact kuny137 : " + err.message));
+}
+
+// Upload fichier
+function uploadFile() {
+  const file = document.getElementById("mediaFile").files[0];
+  if (!file) return alert("Aucun fichier sélectionné !");
+  const ref = storage.ref("medias/" + file.name);
+  ref.put(file)
+    .then(() => alert("✅ Fichier uploadé avec succès !"))
+    .catch(err => alert("❌ Échec de l'upload : " + err.message));
+}
